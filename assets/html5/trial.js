bf4659e8c54a43523b95786f99f42f80a54a55e7
Dan Morris <morris.e.dan@gmail.com>
2014-06-22 17:43:25 +1000

    New version of moderniser to add background size class to drop bad background image when not supported.  Polyfill for IE8- to support Array..prototype.map.  Correct Facebook code.  More copy tweaks.

--- assets/html5/trial.js
+++ assets/html5/trial.js
@@ -132,7 +132,6 @@
 			
 			if (typeof filename !== 'undefined') {
 				subject.addClass('text-replaced');
-				title = subject.text().replace(/\s+/g, ' ').trim();
 				newPath = 'assets/img/' + filename + '.png';
 				newImg = $('<img id="repl-' + filename + '" src="' + newPath + '" class="replaced" />');
 				['top','right','bottom','left','center','floor'].map(function(el,ix){ if (subject.hasClass(el)) { newImg.addClass(el); } });
@@ -154,3 +153,36 @@
 	
 })(jQuery);
 
+// Production steps of ECMA-262, Edition 5, 15.4.4.19
+// Reference: http://es5.github.com/#x15.4.4.19
+if (!Array.prototype.map) {
+  Array.prototype.map = function (callback, thisArg) {
+    var T, A, k;
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }
+    var O = Object(this);
+    var len = O.length >>> 0;
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + " is not a function");
+    }
+    if (thisArg) {
+      T = thisArg;
+    }
+    A = new Array(len);
+    k = 0;
+    while (k < len) {
+
+      var kValue, mappedValue;
+      if (k in O) {
+        var Pk = k.toString(); // This was missing per item a. of the above comment block and was not working in IE8 as a result
+        kValue = O[Pk];
+        mappedValue = callback.call(T, kValue, k, O);
+        A[Pk] = mappedValue;
+      }
+      k++;
+    }
+   // 9. return A
+    return A;
+  };
+}
